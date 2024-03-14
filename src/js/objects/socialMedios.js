13c43d3fca1e58da3243058f40eb96dbed0f1f54
TAVICRACK841 <gustavolizama841@gmail.com>
2024-03-14 14:47:48 -0600

    feat: lslsls

---
+++ src/js/objects/socialMedios.js
@@ -0,0 +1,22 @@
+const socialMedios = [
+    {
+        id: "facebook",
+        url: "",
+        icon: "fa-brands fa-facebook",
+    },
+    {
+        id: "twitch",
+        url: "https://www.twitch.tv/tavicrack",
+        icon: "fa-brands fa-twitch",
+    },
+    {
+        id: "youtube",
+        url: "https://www.youtube.com/channel/UCObQZoyumZM1kFJ6lrtrv2A",
+        icon: "fa-brands fa-youtube",
+    },
+    {
+        id: "instagram",
+        url: "",
+        icon: "fa-brands fa-instagram",
+    },
+]
