9cb3a228bdec159bdf052bcb595e953c316cf818
TAVICRACK841 <gustavolizama841@gmail.com>
2024-03-14 17:57:31 -0600

    feat: solucion de los problesmas de los iconos

--- src/js/objects/socialMedia.js
+++ src/js/objects/socialMedia.js
@@ -1,24 +1,29 @@
 const socialMedia = [
     {
         id: "facebook",
+        name: "Facebook",
         url: "",
         icon: "fa-brands fa-facebook",
     },
     {
         id: "twitch",
+        name: "Twitch",
         url: "https://www.twitch.tv/tavicrack",
         icon: "fa-brands fa-twitch",
     },
     {
         id: "youtube",
+        name: "Youtube",
         url: "https://www.youtube.com/channel/UCObQZoyumZM1kFJ6lrtrv2A",
         icon: "fa-brands fa-youtube",
     },
     {
         id: "instagram",
+        name: "Instagram",
         url: "",
         icon: "fa-brands fa-instagram",
     },
 ];
+
 
 export default socialMedia;
